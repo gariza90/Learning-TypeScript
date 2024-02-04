752f388595a4f805829e037f255e19eec1fff9eb
gariza90 <gariza90@gmail.com>
2024-02-04 17:35:40 -0500

    Adding basic examples from TypeScript Documentation Webpage

--- 01-StartHere/build/js/main.js
+++ 01-StartHere/build/js/main.js
@@ -6,3 +6,20 @@ let b = 6;
 let c = 2;
 console.log(a / b);
 console.log(c * b);
+const message = "Hello, World!";
+message.toLowerCase();
+const user = {
+    name: "Daniel",
+    age: 26,
+};
+const announcement = "Hello World!";
+announcement.toLocaleLowerCase();
+function flipCoin() {
+}
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+}
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+greet("Yadira", new Date());
--- 01-StartHere/src/main.ts
+++ 01-StartHere/src/main.ts
@@ -3,6 +3,8 @@
   Video: TypeScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
   Link: https://www.youtube.com/watch?v=gieEQFIfgYc&t=38s
 */
+
+// TypeScript infiere el tipo de una variable por su valor asignado
 let username = "ALfred";
 console.log(username);
 
@@ -12,3 +14,48 @@ let c: number = 2;
 
 console.log(a / b);
 console.log(c * b);
+
+/*
+  Ejemplos de documentación.
+  Link: https://www.typescriptlang.org/docs/handbook/2/basic-types.html
+*/
+
+const message = "Hello, World!";
+// Accediendo a la propiedad 'toLowerCase' en 'message' y luego invocarla
+message.toLowerCase();
+// Invocando 'message'
+//message(); // Error de signaturas de llamada
+
+const user = {
+  name: "Daniel",
+  age: 26,
+}
+// Accediendo a una propiedad de objeto
+//console.log(user.location); // Error propiedad no definida
+
+const announcement = "Hello World!";
+// Identificar errores de digitación
+//announcement.toLocaleLowercase();
+//announcement.toLocalLowerCase();
+// Forma deseada
+announcement.toLocaleLowerCase();
+
+function flipCoin() {
+  // Debería ser Math.random()
+  //return Math.random < 0.5; // Error de invocación de función
+}
+
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+  // ...
+} //else if (value === "b") {
+  // Nunca se ejecutará
+//}
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+//greet("Jalenna"); // Argumento faltante
+//greet("Joseph", Date()); // Tipo de argumento no coincidente
+greet("Yadira", new Date());
